0b9da7a08a37d8d141f0fca65b6ad07d114b2032
jfa971 <jeanfrancoisantier@jeans-macbook-pro.home>
2017-06-06 02:27:29 +0000

    Ajout de boutons et affichage du résultat

--- calculationStuff/ViewController.swift
+++ calculationStuff/ViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var display: UILabel?
+    
+    var userIsInTheMiddleOfTyping = false
+    
     @IBAction func touchDigit(_ sender: UIButton) {
-        print ("touchDigit was called")
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDlay = display!.text!
+            display!.text = textCurrentlyInDlay + digit
+        } else {
+            display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        print ("\(digit) was called")
     }
 
 
