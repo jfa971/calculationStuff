857c4423b1ed20fddbe4843bbe31b451bc7fced0
jfa971 <jeanfrancoisantier@jeans-macbook-pro.home>
2017-06-07 01:24:52 +0000

    Fin de la première vidéo

--- calculationStuff/ViewController.swift
+++ calculationStuff/ViewController.swift
@@ -11,22 +11,44 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var display: UILabel?
+    @IBOutlet weak var display: UILabel!
     
     var userIsInTheMiddleOfTyping = false
     
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTyping {
-            let textCurrentlyInDlay = display!.text!
-            display!.text = textCurrentlyInDlay + digit
+            let textCurrentlyInDlay = display.text!
+            display.text = textCurrentlyInDlay + digit
         } else {
-            display!.text = digit
+            display.text = digit
             userIsInTheMiddleOfTyping = true
         }
         print ("\(digit) was called")
     }
 
-
+    var displayValue: Double {
+        get {
+            return  Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue =  Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)
+            default:
+                break
+            }
+        }
+    }
+    
 }
 
